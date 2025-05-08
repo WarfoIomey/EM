c854db081a9f642146b94b7305c2e3e8d02805bb
Kirill Turochkin <kirill180104@mail.ru>
2025-05-08 15:31:18 +0800

    task one

---
+++ task_1.py
@@ -0,0 +1,67 @@
+class ObjList:
+
+    def __init__(self, data, next_item=None, prev_item=None):
+        self.__data = data
+        self.__next_item = next_item
+        self.__prev_item = prev_item
+
+    def set_next(self, obj) -> None:
+        self.__next_item = obj
+
+    def set_prev(self, obj) -> None:
+        self.__prev_item = obj
+
+    @property
+    def next(self):
+        return self.__next_item
+
+    @property
+    def prev(self):
+        return self.__prev_item
+
+    def set_data(self, data) -> None:
+        self.__data = data
+
+    @property
+    def data(self):
+        return self.__data
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_obj(self, obj) -> None:
+        """Добавление нового объекта в конец связного списка."""
+        if self.tail is not None:
+            self.tail.set_next(obj)
+            obj.set_prev(self.tail)
+            self.tail = obj
+        else:
+            self.head = self.tail = obj
+
+    def remove_obj(self) -> None:
+        """Удаление последнего объекта из связного списка."""
+        current_obj = self.tail
+        prev_obj = current_obj.prev
+        prev_obj.set_next(current_obj.next)
+
+    def get_data(self) -> list:
+        """Получение списка из строк со всех объектов связного списка."""
+        current_obj = self.head
+        result = []
+        while current_obj is not None:
+            result.append(current_obj.data)
+            current_obj = current_obj.next
+        return result
+
+
+if __name__ == '__main__':
+    lst = LinkedList()
+    lst.add_obj(ObjList('Данные 1'))
+    lst.add_obj(ObjList('Данные 2'))
+    lst.add_obj(ObjList('Данные 3'))
+    res: list = lst.get_data()
+    print(res)
