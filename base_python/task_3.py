668d2569e2002a268d7fa6ef99f44fb95736ffd1
Kirill Turochkin <kirill180104@mail.ru>
2025-05-10 11:03:26 +0800

    fix

--- base_python/task_3.py
+++ base_python/task_3.py
@@ -2,15 +2,24 @@ class Server:
     """Работа серверов в сети."""
     _counter = 0
 
-    def __init__(self, router=None):
+    def __init__(self, router=None, buffer=[]):
         Server._counter += 1
         self.ip_address = Server._counter
         self.router = router
+        self.buffer = buffer
         self.packegs = []
 
-    def send(self, data, server):
+    def send(self, data):
         if self.router is not None:
-            router.buffer.append((data, server))
+            router.buffer.append((data.data, data.server))
+
+    def get_ip(self):
+        return self.ip_address
+
+    def get_data(self):
+        data = self.packegs
+        self.packegs = []
+        return data
 
     def __str__(self):
         return f'IP-адрес сервера: {self.ip_address}'
@@ -19,9 +28,9 @@ class Server:
 class Router:
     """Работа роутера в сети."""
 
-    def __init__(self, servers=[]):
+    def __init__(self, servers=[], buffer=[]):
         self.servers = servers
-        self.buffer = []
+        self.buffer = buffer
 
     def link(self, server):
         self.servers.append(server)
@@ -40,21 +49,24 @@ class Router:
 class Data:
     """Пакеты информации."""
 
-    def __init__(self, data):
+    def __init__(self, data, server):
         self.data = data
+        self.server = server
 
 
-if __name__ == '__main__':
-    sv = Server()
-    sv2 = Server()
-    router = Router()
-    router.link(sv)
-    router.link(sv2)
-    data = Data('Hello my friend')
-    data_two = Data('dont hello')
-    sv.send(data, sv2)
-    sv.send(data_two, sv2)
-    print(router.buffer)
-    router.send_data()
-    print(router.buffer)
-    print(sv2.packegs)
+# if __name__ == '__main__':
+    # sv = Server()
+    # sv2 = Server()
+    # router = Router()
+    # router.link(sv)
+    # router.link(sv2)
+    # data = Data('Hello my friend', sv2)
+    # data_two = Data('dont hello', sv2)
+    # sv.send(data)
+    # sv.send(data_two)
+    # print(router.buffer)
+    # router.send_data()
+    # print(router.buffer)
+    # print(sv2.packegs)
+    # print(sv2.get_data())
+    # print(sv2.get_data())
