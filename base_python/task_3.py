71c8414da556a266cf1a4ef7e40f29b78f2512d6
Kirill Turochkin <kirill180104@mail.ru>
2025-07-12 13:15:46 +0800

    Added __repr__ + fix bug for send messages

--- base_python/task_3.py
+++ base_python/task_3.py
@@ -2,47 +2,56 @@ class Server:
     """Работа серверов в сети."""
     _counter = 0
 
-    def __init__(self, router=None, buffer=[]):
+    def __init__(self, router=None, buffer=None):
         Server._counter += 1
         self.ip_address = Server._counter
         self.router = router
-        self.buffer = buffer
+        self.buffer = buffer if buffer is not None else []
         self.packegs = []
 
     def send(self, data):
-        if self.router is not None:
+        if (
+            self.router is not None
+            and data.server is not None
+            and data.server.router is not None
+        ):
             self.router.buffer.append((data.data, data.server))
 
     def get_ip(self):
         return self.ip_address
 
     def get_data(self):
-        data = self.packegs
+        data = self.packegs.copy()
         self.packegs = []
         return data
 
     def __str__(self):
         return f'IP-адрес сервера: {self.ip_address}'
 
+    def __repr__(self):
+        return f'Сервер {self.ip_address}'
+
 
 class Router:
     """Работа роутера в сети."""
 
-    def __init__(self, servers=[], buffer=[]):
-        self.servers = servers
-        self.buffer = buffer
+    def __init__(self, servers=None, buffer=None):
+        self.servers = servers if servers is not None else []
+        self.buffer = buffer if buffer is not None else []
 
     def link(self, server):
         self.servers.append(server)
         server.router = self
 
     def unlink(self, server):
-        self.servers.remove(server)
-        server.router = None
+        if server in self.servers:
+            self.servers.remove(server)
+            server.router = None
 
     def send_data(self):
-        for g in self.buffer:
-            self.servers[self.servers.index(g[1])].packegs.append(g[0])
+        for data in self.buffer:
+            if data[1] in self.servers:
+                data[1].packegs.append(data[0])
         self.buffer = []
 
 
